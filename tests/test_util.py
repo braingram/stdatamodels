49e1054c4813bfb3b78a30fd3fb5592cd826678c
Brett <brettgraham@gmail.com>
2023-09-22 14:11:00 -0400

    working on unicode vs str handling

--- tests/test_util.py
+++ tests/test_util.py
@@ -215,7 +215,7 @@ def test_gentle_asarray_structured_dtype_configurations(
     """
     # start with a target dtype which should (if no error occurs) be the dtype of the result
     target_dtype = np.dtype(
-        [("i", "i4"), ("f", "f8"), ("s", "S3"), ("b", "bool"), ("u", "uint8"), ("e", "i4")]
+        [("i", "i4"), ("f", "f8"), ("s", "U3"), ("b", "bool"), ("u", "uint8"), ("e", "i4")]
     )
     input_descr = target_dtype.descr
     if extra_columns:
@@ -244,7 +244,7 @@ def test_gentle_asarray_structured_dtype_configurations(
     input_array = np.zeros(5, input_dtype)
     input_array["i"] = 2
     input_array["f"] = 0.1
-    input_array["s"] = b"a"
+    input_array["s"] = "a"
     input_array["b"] = True
     input_array["u"] = 3
     if change_case:
@@ -262,8 +262,8 @@ def test_gentle_asarray_structured_dtype_configurations(
     # check data passed through correctly
     assert np.all(new_array["i"] == 2)
     assert np.allclose(new_array["f"], 0.1)
-    assert np.all(new_array["s"] == b"a")
-    assert np.all(new_array["b"])
+    assert np.all(new_array["s"] == "a")
+    assert np.all(new_array["b"] == True)
     assert np.all(new_array["u"] == 3)
     if not extra_columns:
         # if we have not extra columns, the output dtype should match the target
